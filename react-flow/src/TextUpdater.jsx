ef187cf15245a2569a7b838cb3c49413c64e05a3
Rzq_0 <rizkywirajaya946@gmail.com>
2025-10-29 10:25:10 +0800

    first commit

---
+++ react-flow/src/TextUpdater.jsx
@@ -0,0 +1,14 @@
+export function TextUpdaterNode(props) {
+  const onChange = useCallback((evt) => {
+    console.log(evt.target.value);
+  }, []);
+ 
+  return (
+    <div className="text-updater-node">
+      <div>
+        <label htmlFor="text">Text:</label>
+        <input id="text" name="text" onChange={onChange} className="nodrag" />
+      </div>
+    </div>
+  );
+}
